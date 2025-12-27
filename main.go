0bc71484b815033dc4dcf1b61a40a6aa5433ea63
VitorMakiyama <joemakiyama@gmail.com>
2025-12-26 22:40:15 -0300

    Created basic logic
    Implemented a simple Grinder and a pool of machines that can be used

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Grinder struct {}
+
+func Grind(grinders chan Grinder, orderID int, results chan<- int) {
+	m := <- grinders
+	grindingTime := time.Duration(1)
+	time.Sleep(grindingTime * time.Second)
+	fmt.Println(fmt.Sprintf("Grinded beans: %d", orderID))
+	results <- orderID
+	grinders <- m
+}
 
 func main() {
-	fmt.Println("Hello World")
+	// Creating my Grinder machines
+	numberOfGrinders := 2
+	grinders := make(chan Grinder, numberOfGrinders)
+	for _ = range numberOfGrinders {
+		grinders <- Grinder{}
+	}
+
+	numberOfOrders := 100
+	results := make(chan int)
+	for i := range numberOfOrders {
+		go Grind(grinders, i, results)
+	}
+	for counter := 0; counter < 100; counter++ {
+		<- results
+	}
 }
