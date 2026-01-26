f3b8237af9cb2cfabd71eae6e5c7e0454e616674
VitorMakiyama <joemakiyama@gmail.com>
2026-01-26 13:19:13 -0300

    Fix racing condition on 'traces' map
    Fixed 'fatal error: concurrent map read and map write' (on telemetry.go -> AddSpan(...)) using chan

--- telemetry/telemetry.go
+++ telemetry/telemetry.go
@@ -18,30 +18,35 @@ type Span struct {
 
 func NewTelemetryService() TelemetryService {
 	t := TelemetryService{
-		traces: make(map[int][]Span),
+		traces: make(chan map[int][]Span, 1),
 	}
+	t.traces<- make(map[int][]Span)
 	return t
 }
 
 type TelemetryService struct {
-	traces map[int][]Span
+	traces chan map[int][]Span
 }
 
 func (t *TelemetryService) AddSpan(id int, ts time.Time, action string) {
+	traces := <-t.traces
+	trace := traces[id]
+	
 	newSpan := Span{
 		id: id,
 		ts: ts,
 		action: action,
 	}
-	trace := t.traces[newSpan.id]
 
 	trace = append(trace, newSpan)
-	t.traces[newSpan.id] = trace
+	traces[newSpan.id] = trace
+	t.traces<- traces
 }
 
 func (t *TelemetryService) PrintTelemetry() {
+	traces := <-t.traces
 	//TODO: Melhorar o print
-	for _, t := range t.traces {
+	for _, t := range traces {
 		for _, s := range t {
 			fmtText := fmt.Sprintf("id: %d, timestamp (Unix): %d, ts.DateTime: %s, action: %s", s.id, s.ts.Unix(), s.ts.Format(time.DateTime), s.action)
 			fmt.Println(fmtText)
